fa09db86fe98262872bbf440135a496d19c3b6f6
nirelko <nirelko1@gmail.com>
2018-01-11 09:58:13 +0200

    Started to add the country list

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import AsosManager from './managers/asos-manager';
 
-new AsosManager().getProductDetails('4601984')
+new AsosManager().getProductDetails('http://m.asos.com/es/adidas-originals/zapatillas-de-deporte-blancas-bb0070-stan-smith-de-adidas-originals/prd/8268038?xaffid=12900&r=1')
   .then(x => {
     debugger;
   })
--- server/managers/asos-manager.js
+++ server/managers/asos-manager.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
 class AsosManager {
-  constructor() {
+  constructor () {
     this.client = axios.create({
       baseURL: 'http://m.asos.com/api/'
     });
+
+    this.extractPidFromUrl = this.extractPidFromUrl.bind(this);
+    this.getProductDetails = this.getProductDetails.bind(this);
   }
 
-  getProductDetails(pid, { store: store = 'COM', currency: currency = 'USD' } = { store: 'COM', currency: 'USD' }) {
-    return this.client.get(`/product/catalogue/v2/stockprice?productIds=${pid}&store=${store}&currency=${currency}&keyStoreDataversion=2&sizeSchema=US&country=USD`)
+  extractPidFromUrl (url) {
+    const pidStartIndex = url.indexOf('prd/') + 4;
+
+    return url.substring(pidStartIndex, url.indexOf('?', pidStartIndex));
+  }
+
+  getProductDetails(url, { store, currency, sizeSchema, country}) {
+    return this.client.get(`/product/catalogue/v2/stockprice?productIds=${this.extractPidFromUrl(url)}&store=${store}&currency=${currency}&keyStoreDataversion=2&sizeSchema=${sizeSchema}&country=${country}`)
       .then(x => x.data);
   }
 }
